db6f2a6d47254fba54cef49adba38e25dc522f42
Mike Jaren Yap <mjaren.yap@gmail.com>
2021-08-27 00:20:48 +0800

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/layout";
+
+const AboutPage = () => {
+  return (
+    <Box>
+      <Text>hello</Text>
+    </Box>
+  );
+};
+
+export default AboutPage;
